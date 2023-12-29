0e395e0b462cbc9b5c988c8c75cfd5d3763304a4
Wai Yan Latt <humanant67@gmail.com>
2023-12-29 11:29:51 +0630

    dsuu

---
+++ assets/components/Exp.js
@@ -0,0 +1,58 @@
+const Exp = {
+  template: `
+    <div class="exp">
+    <h1 class="about-font">
+      <span class="A01">02.</span>Where I've Worked
+    </h1>
+    <div class="exp-flex">
+      <div class="exp-card">
+        <h1 class="exp-font">
+          Frontend/UI Developer
+          <span class="text-green">@ Epic Startup Tech Company</span>
+        </h1>
+        <p class="font">March 2021 - April 2022</p>
+        <ul class="skill-list">
+          <li>
+            Write modern, performant, maintainable code for a diverse array
+            of projects.
+          </li>
+          <li>
+            Work with a variety of different languages, platforms,
+            frameworks, and plugins such as JavaScript, Tailwind, SASS,
+            Sveltekit.
+          </li>
+          <li>
+            Communicate with multi-disciplinary teams of developers,
+            designers and product manager.
+          </li>
+        </ul>
+      </div>
+      <div class="exp-card mr-11">
+        <h1 class="exp-font">
+          Instructor
+          <span class="text-green">@ The Perfect 404</span>
+        </h1>
+        <p class="font">September 2022 - Present</p>
+        <ul class="skill-list">
+          <li>
+            Make tutorial videos on both Facebook and YouTube on how to
+            code.
+          </li>
+          <li>
+            Create beginner-friendly web development course structure and
+            assessment project that are easy to learn for our tech students.
+          </li>
+          <li>
+            Enhancing our SprintX web app by improving the UI, adding more
+            courses, and planning to integrate an <span class="text-green">AI chat</span> for a better
+            learning experience for our students.
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+    `,
+};
+
+
+export default Exp;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 import Navbar from "/assets/components/Navbar.js";
 import Home from "/assets/components/Home.js";
 import About from "/assets/components/About.js";
+import Exp from "/assets/components/Exp.js";
 
 const { createApp } = Vue;
 // Vue Instance
@@ -9,6 +10,7 @@ const app = createApp({
     'navbar-component' : Navbar,
     'home-component' : Home,
     'about-component' : About,
+    'exp-component' : Exp,
   },
   data() {
     return {
